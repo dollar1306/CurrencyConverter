2f277ab96dc2841fe9a76f989e21abffed6f2c06
dollar1306 <dollar1306@gmail.com>
2021-03-01 17:34:14 +0200

    project is running :)))

--- src/main/java/RunConverter.java
+++ src/main/java/RunConverter.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+
 
 public class RunConverter {
 
--- src/main/java/WriteToResult.java
+++ src/main/java/WriteToResult.java
@@ -1,41 +1,20 @@
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class WriteToResult {
-
-//    static void writeToLog(double input,double value ) throws IOException {
-//        BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Alex" +
-//                "\\IdeaProjects\\CurrencyConverter\\src\\main\\java\\results.txt"));
-//        writer.write(input+" = "+ value+"\n");
-//        writer.close();
-//    }
-
     public static void writeToLog(ArrayList<Result> convertList) throws IOException {
          BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Alex\\IdeaProjects\\CurrencyConverter\\src\\main\\java\\results.txt",true));
          for(Result i :convertList) {
              try {
-                 //writer.write(String.valueOf(convertList)+"\n");
                  writer.write((i.getValue() + " , " + i.getConvertType())+"\n");
-             } catch (IOException e) {
+             } catch (FileNotFoundException e) {
                  e.printStackTrace();
+                 System.out.println("An error occurred.");
              }
          }
          writer.close();
-//        try {
-//            File resultFile = new File("C:\\Users\\Alex\\IdeaProjects\\CurrencyConverter\\src\\main\\java\\results.txt");
-//            Scanner myReader = new Scanner(resultFile);
-//            System.out.println("Your convert history ");
-//            while (myReader.hasNextLine()) {
-//                String data = myReader.nextLine();
-//                System.out.println(data);
-//            }
-//            myReader.close();
-//        }catch (FileNotFoundException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
-       //ReadFile.readFile(str);
     }
 }
