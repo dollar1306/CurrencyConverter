ca8c0ede8e8eb84ace66f8efd689471b333d3db0
dollar1306 <dollar1306@gmail.com>
2021-03-02 16:44:01 +0200

    add try&catch to second screen

--- src/main/java/SecondScreen.java
+++ src/main/java/SecondScreen.java
@@ -1,9 +1,15 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SecondScreen {
-    public static double secondScreen() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter amount for convert");
-        return scan.nextDouble();
+    public static double secondScreen(){
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter amount for convert");
+            return scan.nextDouble();
+        }catch (InputMismatchException in){
+            System.out.println("Your input amount not numbers, please try again");
+            return SecondScreen.secondScreen();
+        }
     }
 }
